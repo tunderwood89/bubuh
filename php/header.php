ee64ee9b13d85733ac77793043f6a728e89ef220
tunderwood89 <108191238+tunderwood89@users.noreply.github.com>
2024-08-05 07:21:21 +0000

    changing repos soon (on here not github)

---
+++ php/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header-container">
+        <div class="title-tagline">
+            <a href="index.html"><h1 class="site-title">bubuh</h1></a>
+            <p class="tagline">bubuh is tom's blog</p>
+        </div>
+        <div class="links-all">
+            <a href="https://bio.link/tunderwood"><p class="links-text">my links</p></a>
+        </div>
+    </div>
+</header>
